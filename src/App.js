b0cac1286e4ca5b09661fbad663b029073a0d136
Theeradach subsin <theeradach_s@symenergy.co.jp>
2021-04-01 10:50:57 +0900

    edit message on App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This Pages was created for Github Deployment Practice. 
         </p>
         <a
           className="App-link"
